6f95dc0e40808735b89515e46a8639028fa20f77
itsyaboikris <kristoff_riley@outlook.com>
2023-04-16 08:19:54 -0400

    wip theme switch

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header'
 import './globals.css'
+import Providers from './Providers'
 
 export const metadata = {
   title: 'IMDb Clone',
@@ -13,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       	<body>
-			<Header />
+			{/* <Providers> */}
+				<Header />
 
-			{children}
+				{children}
+			{/* </Providers> */}
 		</body>
     </html>
   )
