13155b1be8ae74177cafaaaf285043f4fae41114
itsyaboikris <kristoff_riley@outlook.com>
2023-04-16 15:21:53 -0400

    fetch movies from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,28 @@
-import Image from 'next/image'
+import Results from '@/components/Results';
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const API_KEY = process.env.API_KEY
 
-export default function Home() {
-  return (
-    <main className="">
-      
-    </main>
-  )
+export default async function Home({searchParams}) {
+
+	const genre = searchParams.genre || "fetchTrending"
+
+	const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,{
+		next : {
+			revalidate: 10000
+		}
+	});
+
+	const data = await res.json();
+
+	const results = data.results;
+
+	console.log(results)
+
+	return (
+		<div className="">
+			<Results results={results} />
+		</div>
+	)
 }
